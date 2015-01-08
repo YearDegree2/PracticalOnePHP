68e4b85e2d36e66efbc8cf6bc72106329a12e4ef
nicobascon <nico.bascon@live.fr>
2015-01-08 20:00:46 +0100

    Data Store

--- model/cities.php
+++ model/cities.php
@@ -1,6 +1,6 @@
 <?php
 
-$cities = array(
+$citiesArray = array(
     [ "name" => "San Francisco", "country" => "USA"       ],
     [ "name" => "Paris",         "country" => "France"    ],
     [ "name" => "New York",      "country" => "USA"       ],
@@ -11,3 +11,21 @@ $cities = array(
     [ "name" => "Bombay",        "country" => "India"     ],
     [ "name" => "Vancouver",     "country" => "Canada"    ],
 );
+
+// recover data fro the csv file
+$citiesFile = fopen(__DIR__ .'/../csv/cities.csv', 'r');
+if ($citiesFile) {
+	$citiesFinal = array();
+	while (($data = fgetcsv($citiesFile, 1000, ";")) !== FALSE) {
+        $citiesCSV = array(
+	        "name"     => $data[0],
+	        "country"  => $data[1],
+        );
+        array_push($citiesFinal, $citiesCSV);
+    }
+    fclose($citiesFile);
+    $cities = $citiesFinal;
+}
+
+// recover data from the array
+// $cities = $citiesArray;
