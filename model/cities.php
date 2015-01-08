0e78f15e0ca2b1808e614aa649711377083e09d5
nicobascon <nico.bascon@live.fr>
2015-01-08 20:24:07 +0100

    Modify Data Store

--- model/cities.php
+++ model/cities.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once __DIR__ . "/functions.php";
+
 $citiesArray = array(
     [ "name" => "San Francisco", "country" => "USA"       ],
     [ "name" => "Paris",         "country" => "France"    ],
@@ -12,20 +14,13 @@ $citiesArray = array(
     [ "name" => "Vancouver",     "country" => "Canada"    ],
 );
 
-// recover data fro the csv file
-$citiesFile = fopen(__DIR__ .'/../csv/cities.csv', 'r');
-if ($citiesFile) {
-	$citiesFinal = array();
-	while (($data = fgetcsv($citiesFile, 1000, ";")) !== FALSE) {
-        $citiesCSV = array(
-	        "name"     => $data[0],
-	        "country"  => $data[1],
-        );
-        array_push($citiesFinal, $citiesCSV);
-    }
-    fclose($citiesFile);
-    $cities = $citiesFinal;
-}
+// recover data from the csv file
+$citiesCSV = csvToArray(__DIR__ . '/../csv/cities.csv', ';');
 
-// recover data from the array
-// $cities = $citiesArray;
+if (null === $citiesCSV) {
+    // recover data from the array
+    $cities = $citiesArray;
+}
+else {
+    $cities = $citiesCSV;
+}
---
+++ model/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function csvToArray ($csvFile, $separator)
+{
+	$citiesFile = fopen($csvFile, 'r');
+	if ($citiesFile) {
+		$cities = array();
+		while (($data = fgetcsv($citiesFile, 1000, $separator)) !== FALSE) {
+	        $city = array(
+		        "name"     => $data[0],
+		        "country"  => $data[1],
+	        );
+	        array_push($cities, $city);
+	    }
+    	fclose($citiesFile);
+    	return $cities;
+	}
+	return null;
+}
